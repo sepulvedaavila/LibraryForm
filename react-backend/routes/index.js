2672f017e4022425c9cfbfed9a04e9e022bca24d
Mariano Geyne <marianogeyne@Marianos-MacBook-Pro.local>
2018-10-19 11:34:44 -0500

    ... new repo remote test

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 var express = require('express');
 var cors = require('cors');
+var xmlbuilder = require('xmlbuilder');
+var fs = require('fs');
 var router = express.Router();
 
 router.use(cors());
@@ -11,16 +13,46 @@ router.get('/', function(req, res, next) {
 });
 
 /*  POST OMDB Service */
-router.POST('/getMovie', function (req, res) {
+/* Papaw en Express las funciones de router son con minúsculas */
+router.get('/getMovie', function (req, res) {
   // Papaw los campos de los POST se acceden con req.query.CAMPO
+  console.log(req.query.title);
   fetch('http://www.omdbapi.com/?t=' + req.query.title + '&apikey=82a4df75')
     .then(results => {
       return results.json();
-    }).then(data => {
-      console.log(data);
-      // Aqui crear XML y guardarlo en Server
-      res.send(data);
+    }).then(data => {   
+
+      console.log(data.Response);
+            if(data.Response === 'False'){              
+                console.log("No existe la pelicula en la OMDB");
+                res.sendStatus(300);
+            }else{
+                var xml = xmlbuilder.create({
+                    root: {
+                        peliculas: {
+                            titulo: {
+                                '@type': 'Titulo', 
+                                '#text': data.Title 
+                            }
+                        }
+                    }
+                });
+                console.log(xml);
+                // el modulo fs con el metodo writeFile solo acepta como segundo paramentro un Buffer y un string
+                // así que hay que convertir el xml a un string para que sea procesado.
+                xml = xml.toString({pretty:true});
+                
+                fs.writeFile('public/xml/'+data.Title+'.xml', xml, function(err){
+                  if(err) console.log(err);
+                  console.log("archivo guardado");
+                  res.send(data);
+                });
+              
+              }
+    }).catch( (err) => {
+        console.log(err);
+        res.send("Error: no se ha podido conectar con la API");
     });
-});
+  });
 
 module.exports = router;
