c3fc36ce2678e34ac5457fe6da7dd5743a3fdb90
Mariano Geyne <marianogeyne@Marianos-MacBook-Pro.local>
2018-12-14 12:51:08 -0600

    correciones en JQuery videograbaciones.js

--- react-backend/public/scripts/videograbaciones.js
+++ react-backend/public/scripts/videograbaciones.js
@@ -6,7 +6,10 @@
             $.get('http://www.omdbapi.com/', {t:titulo, apikey:'82a4df75'}, function(data){
                 console.log(data);
                 if(data.hasOwnProperty('Error')){
-                    swal(data.Error);
+                    swal({type: 'error',
+                        title: 'Error en API',
+                        text: 'La pélicula que estas buscando no se encuentra en la API, verifica el titulo'
+                    });
                 } else {
                     $('#titulo').val(data.Title);
                     $('#nota_resumen').val(data.Plot);
@@ -20,10 +23,38 @@
     });
 
             $("#addGeneral").click(function(){
-                $("#divNotaGeneral").append('<div><input class="short-inputs" name="nota_general1" type="text" placeholder="etiqueta"><input class= "short-inputs" name = "nota_general1" type = "text" placeholder = "Indice" ><input name="nota_general1" type="text" placeholder="Type something"><input type="button" value="-" class="remove"><br/></div>');
+                $("#divNotaGeneral")
+                .append('<div="row-labels-and-inputs">'+
+                            '<div class="row">'+
+                                '<div class="input-group input-group-sm mb-3">'+
+                                    '<div class="input-group-prepend col-md-2">'+
+                                        '<span>Nota General</span>'+
+                                    '</div>'+
+                                    '<label class="col-md-1 mr-2" name="nota_general1" type="text">490/500</label>'+
+                                    '<input class="form-control col-md-1 mr-2" name="nota_premios1" type="text" placeholder="etiqueta">'+
+                                    '<input class="form-control col-md-1 mr-2" name = "nota_premios1" type = "text" placeholder = "Indice" >'+
+                                    '<input class="form-control col-md-6" name="nota_premios1" type="text" placeholder="Type something">'+
+                                    '<input class="fa fa-minus btn btn-light remove" type="button">'+
+                                '</div>'+
+                            '</div>'+
+                        '<br/></div>');
             });
             $("#addPremios").click(function () {
-                $("#divNotaPremios").append('<div><input class="short-inputs" name="nota_premios1" type="text" placeholder="etiqueta"><input class= "short-inputs" name = "nota_premios1" type = "text" placeholder = "Indice" ><input name="nota_premios1" type="text" placeholder="Type something"><input type="button" value="-" class="remove"><br/></div>');
+                $("#divNotaPremios")
+                .append('<div="row-labels-and-inputs">'+
+                            '<div class="row">'+
+                                '<div class="input-group input-group-sm mb-3">'+
+                                    '<div class="input-group-prepend col-md-2">'+
+                                        '<span>Nota de Premios</span>'+
+                                    '</div>'+
+                                    '<label class="col-md-1 mr-2" name="nota_general1" type="text">586</label>'+
+                                    '<input class="form-control col-md-1 mr-2" name="nota_premios1" type="text" placeholder="etiqueta">'+
+                                    '<input class="form-control col-md-1 mr-2" name = "nota_premios1" type = "text" placeholder = "Indice" >'+
+                                    '<input class="form-control col-md-6" name="nota_premios1" type="text" placeholder="Type something">'+
+                                    '<input class="fa fa-minus btn btn-light remove" type="button">'+
+                                '</div>'+
+                            '</div>'+
+                        '<br/></div>');
             });
             $("#createXML").click(function(){
                 var inputValues = [];
