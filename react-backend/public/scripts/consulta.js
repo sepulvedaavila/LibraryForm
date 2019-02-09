8684b61fc0053661babe3458767563abd89b7e75
Carlos Sepulveda <cnsa1606@gmail.com>
2019-02-09 16:58:31 -0600

    Retrieving data from local db

--- react-backend/public/scripts/consulta.js
+++ react-backend/public/scripts/consulta.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
     $('#findFilm').click(function(){
         let titulo = $('#titulo').val();
-        /* 
-        TODO: 
-            Call internal service that checks 
-            for the movie's title in the DB
-        */
+        $.get('consulta/search'+titulo, function(data){
+            console.log(data.data);
+        });
     });
 });
--- react-backend/routes/consulta/index.js
+++ react-backend/routes/consulta/index.js
@@ -3,11 +3,31 @@ var express = require('express');
 var cors = require('cors');
 var xmlbuilder = require('xmlbuilder');
 var fs = require('fs');
+var mongoose = require('mongoose');
+var modelPeliculas = require('../../models/peliculas');
 var router = express.Router();
 
 router.use(cors());
 
 var baseRoute = "/consulta";
+
+
+router.get('/search:title', function (req, res) {
+    var title = req.params.title;
+    modelPeliculas.find({Title:title},function(err,data){
+        if(err){ 
+            //console.log(err);
+            console.log('Si hay ERRNO');
+        } else {
+            console.log('No hay ERRNO');
+            var titles = data.map(elem=>{
+                return elem.Title;
+            });
+            console.log(titles);
+            res.status(200).send( { data: titles });
+        }
+    });
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
